b7903f90514cde4c1e41961c2926f8ac14ca693e
HP <HP@LAPTOP>
2024-05-21 17:24:54 +0530

    ll

---
+++ sele/src/test/java/selep/ContavtTest.java
@@ -0,0 +1,25 @@
+package selep;
+
+import org.testng.annotations.Test;
+
+public class ContavtTest {
+	
+	@Test
+	public void getContactTest()
+	{
+		System.out.println("execute con");
+	}
+	
+	@Test
+	public void getContactTestw()
+	{
+		System.out.println("execute con");
+	}
+	
+	@Test
+	public void modifyTest()
+	{
+		System.out.println("modify con");
+	}
+
+}
---
+++ sele/src/test/java/selep/OrgTest.java
@@ -0,0 +1,21 @@
+package selep;
+
+import org.testng.annotations.Test;
+
+public class OrgTest{
+
+	
+	@Test
+	public void getOrgTest()
+	{
+		System.out.println("execute org");
+	}
+	
+	@Test
+	public void modifyOrgTest()
+	{
+		System.out.println("modify org ");
+	}
+
+	
+}
